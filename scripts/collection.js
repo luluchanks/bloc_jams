ef397cbee1482b913b4550a9b0032e2fa057dd66
Luwin Changco <luwinchangco@gmail.com>
2017-04-22 14:38:53 -0400

    Checkpoint 30 Collection and Album Views Completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,26 +1,33 @@
-var collectionItemTemplate =
-    '<div class="collection-album-container column fourth">'
-  + '  <img src="assets/images/album_covers/01.png"/>'
-  + '  <div class="collection-album-info caption">'
-  + '    <p>'
-  + '      <a class="album-name" href="album.html"> The Colors </a>'
-  + '      <br/>'
-  + '      <a href="album.html"> Pablo Picasso </a>'
-  + '      <br/>'
-  + '      X songs'
-  + '      <br/>'
-  + '    </p>'
-  + '  </div>'
-  + '</div>'
-  ;
+var buildCollectionItemTemplate = function() {
+     var template =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+    ;
 
-window.onload = function() {
+     return $(template);
+ };
 
-  var collectionContainer = document.getElementsByClassName('album-covers')[0];
+ $(window).load(function() {
 
-  collectionContainer.innerHTML = '';
+     var $collectionContainer = $('.album-covers');
 
-  for (var i = 0; i < 12; i++) {
-      collectionContainer.innerHTML += collectionItemTemplate;
-  }
-}
+     $collectionContainer.empty();
+
+     for (var i = 0; i < 12; i++) {
+         var $newThumbnail = buildCollectionItemTemplate();
+
+         $collectionContainer.append($newThumbnail);
+     }
+
+ });
